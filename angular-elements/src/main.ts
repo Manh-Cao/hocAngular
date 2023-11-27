502e8c74b7919375a26916227079685095e481e0
Cao Manh <manhcv@imt-soft.com>
2023-11-27 11:05:28 +0700

    first commit

---
+++ angular-elements/src/main.ts
@@ -0,0 +1,15 @@
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {provideAnimations} from '@angular/platform-browser/animations';
+
+import {AppComponent} from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideAnimations()],
+}).catch((err) => console.error(err));
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
