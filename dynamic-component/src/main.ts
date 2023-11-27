f547fcd6f8e3a2927a9da7d60f9d04c5d9de1d32
Cao Manh <manhcv@imt-soft.com>
2023-11-27 11:35:08 +0700

    dynamic component commit

---
+++ dynamic-component/src/main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AdBannerComponent } from './app/ad-banner.component';
+
+bootstrapApplication(AdBannerComponent);
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
