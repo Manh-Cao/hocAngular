bce125680487b768cdfa6512fe85d4192f52b885
Cao Manh <manhcv@imt-soft.com>
2023-12-01 09:43:36 +0700

    http client commit

--- http-client/src/app/du-lieu.service.ts
+++ http-client/src/app/du-lieu.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {  throwError, catchError } from 'rxjs';
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 
 
 @Injectable({
@@ -8,9 +9,27 @@ import { HttpClient } from '@angular/common/http';
 export class DuLieuService {
 
   constructor(private h:HttpClient) { }
-  getSanPham(){
-    return this.h.get('http://localhost:3000/sanpham');
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
+
+  getSanPham(){
+    return this.h.get('http://localhost:3000/sanpham').pipe(
+      catchError(this.handleError)
+    );
+  }
+
 
   themSanPham(sp:any){
     return this.h.post('http://localhost:3000/sanpham',sp);
